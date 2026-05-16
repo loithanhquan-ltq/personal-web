1c39170ce3111cb4bb91483175384a04c256743e
Loi Quan <loiquan@MacBook-Pro-cua-Loi.local>
2026-05-16 21:17:07 +0700

    change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1] || "personal-web";
+const REPO_NAME = "personal-web";
+const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1] || REPO_NAME;
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
